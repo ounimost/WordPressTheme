590489307ee29afdbe34794690b05ddd4cf7515c
ouni <>
2021-08-18 08:22:18 +0900

    記事一覧を表示させるように

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
   <title>My WordPress Theme</title>
 </head>
 <body>
-  これは、はじめてのWordPressテーマです。<br>test.<br>test.
+  <h1>記事の一覧</h1>
+
+	<div>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();　?>
+        <div>
+          <hr>
+          <h3><a href="<?php echo get_permalink(); ?>"><?php echo $post->post_title; ?></a></h3>
+					<p><a href=""><?php the_category(', '); ?></a></p>
+					<timedatetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d'); ?></timedatetime=>
+				</div>
+			<?php endwhile; endif;　?>
+		</div>
+	</div>
 </body>
 </html>
