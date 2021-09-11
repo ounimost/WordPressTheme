b1f0364f1b49f06c94a89e403575ca931d4aaa88
ouni <>
2021-09-11 17:09:40 +0900

    投稿一覧をGridにする

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
 <body>
   <h1>記事の一覧</h1>
 
-  <div>
+  <div id="container">
       <!-- 投稿の数だけ繰り返して処理する -->
       <?php while (have_posts()) : ?>
         
         <!-- 投稿を取得 -->
         <?php the_post(); ?>
 
-        <div>
+        <div id="new-post">
           <hr>
           <!-- 投稿のタイトルを表示して、投稿ページへのリンクを張る -->
           <h3><a href="<?php echo get_permalink(); ?>"><?php echo the_title(); ?></a></h3>
