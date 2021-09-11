ea58e1adc54057fe26781f8fbe145a3f42a5b264
ouni <>
2021-09-11 17:13:26 +0900

    スタイルシートを適用させる

--- functions.php
+++ functions.php
@@ -2,3 +2,9 @@
 
 // サムネイル画像を有効にする
 add_theme_support( 'post-thumbnails' );
+
+// スタイルシートの適用
+function custom_theme_assets() {
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
