430456865848da653f9d368ebd6fa42ea1ac1b38
ouni <>
2021-09-14 20:07:12 +0900

    fontを指定

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>My WordPress Theme</title>
 
+  <!-- Font -->
+  <link href="http://fonts.googleapis.com/earlyaccess/notosansjp.css">
+
   <?php wp_head(); ?>
 </head>
 <body>
