d8bb61a95a8fb3f671791d310a5706d3f1327677
ouni <>
2021-09-17 08:47:30 +0900

    Font Noto sans jp

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <title>MY BLOG</title>
 
   <!-- Font -->
-  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700;900&display=swap" rel="stylesheet">
 
   <?php wp_head(); ?>
 </head>
