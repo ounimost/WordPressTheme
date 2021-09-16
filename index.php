2d4fa5215349e16b3d0793ced3ee3fb6136a1f73
ouni <>
2021-09-17 08:40:48 +0900

    フォントの指定を修正

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My WordPress Theme</title>
+  <title>MY BLOG</title>
 
   <!-- Font -->
-  <link href="http://fonts.googleapis.com/earlyaccess/notosansjp.css">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
 
   <?php wp_head(); ?>
 </head>
