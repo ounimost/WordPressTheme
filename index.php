76ea97ee7c05089627f62d4d7b8917a585267940
ouni <>
2021-09-18 07:37:01 +0900

    footerを追加

--- index.php
+++ index.php
@@ -68,5 +68,13 @@
       </div>
     </div> 
   </main>
+
+  <!------------------------------------------------>
+  <!-- フッター -->
+  <!------------------------------------------------>
+  <footer class="site-footer">
+    <p class="copyright">&copy; 2021 OUNI studio.</p>
+  </footer>
+  <?php wp_footer(); ?>
 </body>
 </html>
