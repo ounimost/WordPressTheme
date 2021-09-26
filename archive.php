d04a2dcafc4a178f2599940329c86e0a040dda26
ouni <>
2021-09-26 16:51:25 +0900

    アーカイブページを追加

---
+++ archive.php
@@ -0,0 +1,35 @@
+<!------------------------------------------------>
+<!-- アーカイブ -->
+<!------------------------------------------------>
+<?php get_header(); ?>
+
+<main id="main-top">
+  <div id="content-area">
+
+    <!-- カテゴリ名の表示 -->
+    <?php $title = single_term_title('', false); ?>
+    <h2 id="top-header">CATEGORY : <?php echo $title; ?></h2>
+
+    <div id="new-post-container">
+        <!-- 投稿の数だけ繰り返して処理する -->
+        <?php while (have_posts()) : ?>
+          
+          <!-- 投稿を取得 -->
+          <?php the_post(); ?>
+
+          <!-- 投稿を表示 -->
+          <div id="new-post">
+            <?php the_post_thumbnail( array( 250, 200 ), array( 'id' => 'new-post-img' ) ); ?>
+            <h3 id="new-post-header"><a href="<?php echo get_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+            <p id="new-post-info">
+              <time id="new-post-date" datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d'); ?></time>
+            </p>
+          </div>
+
+        <?php endwhile; ?>
+      </div>
+    </div>
+  </div> 
+</main>
+
+<?php get_footer(); ?>
